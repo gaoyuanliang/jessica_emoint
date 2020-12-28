eaac5ae981aa2bacdec7e8aa4e548b240237ae31
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2020-12-29 00:01:45 +0400

    Add files via upload

--- jessica_model_train.py
+++ jessica_model_train.py
@@ -1,11 +1,14 @@
 ##############################
 
+from jessica_deep_emotion_sensor import *
+from jessica_emoint_data_conversion import *
+
 fear_texts, fear_tags = convert_file_to_text_and_tag_list(
 	emotion_tag = "fear",
 	data_file = "*.train.txt")
 fear_tagger = train_tagger(texts = fear_texts,
 	tags = fear_tags,
-	tagger_model_path = '/Downloads/fear_tagger.h5',
+	tagger_model_path = 'fear_tagger.h5',
 	epochs = 10)
 '''
 Epoch 10/10
@@ -17,7 +20,7 @@ anger_texts, anger_tags = convert_file_to_text_and_tag_list(
 	data_file = "*.train.txt")
 anger_tagger = train_tagger(texts = anger_texts,
 	tags = anger_tags,
-	tagger_model_path = '/Downloads/anger_tagger.h5',
+	tagger_model_path = 'anger_tagger.h5',
 	epochs = 10)
 '''
 Epoch 10/10
@@ -29,7 +32,7 @@ joy_texts, joy_tags = convert_file_to_text_and_tag_list(
 	data_file = "*.train.txt")
 joy_tagger = train_tagger(texts = joy_texts,
 	tags = joy_tags,
-	tagger_model_path = '/Downloads/joy_tagger.h5',
+	tagger_model_path = 'joy_tagger.h5',
 	epochs = 10)
 '''
 Epoch 10/10
@@ -41,12 +44,14 @@ sadness_texts, sadness_tags = convert_file_to_text_and_tag_list(
 	data_file = "*.train.txt")
 sadness_tagger = train_tagger(texts = sadness_texts,
 	tags = sadness_tags,
-	tagger_model_path = '/Downloads/sadness_tagger.h5',
+	tagger_model_path = 'sadness_tagger.h5',
 	epochs = 10)
 '''
 Epoch 10/10
 3251/3251 [==============================] - 6s 2ms/sample - loss: 0.0212 - acc: 0.9852 - val_loss: 0.2466 - val_acc: 0.9309
 '''
+
+'''
+cp *.h5 /Downloads/
+'''
 ##############################
-
-
